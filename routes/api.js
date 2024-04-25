43da24280ee2b97a29451b8c4eb305f799175351
Dana Arroway <dana.arroway@gmail.com>
2024-04-25 17:33:42 -0500

    Basic functionality of API Initialized without Routing

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const inventoryController = require('../controllers/auth') 
-const reservationsController = require('../controllers/home')
-const usersController = require('../controller/users')
+const reservationsController = require('../controllers/reservations')
+const usersController = require('../controllers/users')
 const ensureAuth = require('../middleware/auth')
 
 //User Endpoints
-router.get('/api/users', ensureAuth, usersController.getUsers)
-router.get('/api/users/:id', ensureAuth, usersController.getUser)
-router.post('/api/users/new', usersController.addUser)
-router.put('/api/users/edit/:id', ensureAuth, usersController.updateUser)
-router.delete('/api/users/delete/:id', ensureAuth, usersController.deleteUser)
+// router.get('/api/users', ensureAuth, usersController.getUsers)
+// router.get('/api/users/:id', ensureAuth, usersController.getUser)
+// router.post('/api/users/new', usersController.addUser)
+// router.put('/api/users/edit/:id', ensureAuth, usersController.updateUser)
+// router.delete('/api/users/delete/:id', ensureAuth, usersController.deleteUser)
 
-//Inventory Endpoints
-router.get('/api/inventory', inventoryController.getInventory)
-router.get('/api/inventory/:id', inventoryController.getItem)
-router.post('/api/inventory/new', ensureAuth, inventoryController.addItem)
-router.put('/api/inventory/edit/:id', ensureAuth, inventoryController.updateItem)
-router.delete('/api/inventory/delete/:id', ensureAuth, inventoryController.deleteItem)
+// //Inventory Endpoints
+// router.get('/api/inventory', inventoryController.getInventory)
+// router.get('/api/inventory/:id', inventoryController.getItem)
+// router.post('/api/inventory/new', ensureAuth, inventoryController.addItem)
+// router.put('/api/inventory/edit/:id', ensureAuth, inventoryController.updateItem)
+// router.delete('/api/inventory/delete/:id', ensureAuth, inventoryController.deleteItem)
 
-//Reservation Endpoints
-router.get('/api/reservations', reservationsController.getReservations)
-router.get('/api/reservations/:id', reservationsController.getReservation)
-router.post('/api/reservations/new', reservationsController.addReservation)
-router.put('/api/reservations/edit/:id', reservationsController.updateReservation)
-router.delete('/api/reservations/delete/:id', ensureAuth, reservationsController.deleteReservation)
+// //Reservation Endpoints
+// router.get('/api/reservations', reservationsController.getReservations)
+// router.get('/api/reservations/:id', reservationsController.getReservation)
+// router.post('/api/reservations/new', reservationsController.addReservation)
+// router.put('/api/reservations/edit/:id', reservationsController.updateReservation)
+// router.delete('/api/reservations/delete/:id', ensureAuth, reservationsController.deleteReservation)
 
 module.exports = router
