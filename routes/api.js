95afc23cdb48d6ae39eddfe919e1d2c86d3d197d
Dana Arroway <92267486+moonlighter-dev@users.noreply.github.com>
2024-05-14 10:11:55 -0500

    adjusted routes

--- routes/api.js
+++ routes/api.js
@@ -3,27 +3,26 @@ const router = express.Router()
 const inventoryController = require('../controllers/inventory') 
 const reservationsController = require('../controllers/reservations')
 const usersController = require('../controllers/users')
-const { ensureAuth } = require('../middleware/auth')
 
 //User Endpoints
-router.get('/api/users', ensureAuth, usersController.getUsers)
-router.get('/api/users/:id', ensureAuth, usersController.getUser)
+router.get('/api/users', usersController.getUsers)
+router.get('/api/users/:id', usersController.getUser)
 router.post('/api/users/new', usersController.addUser)
-router.put('/api/users/edit/:id', ensureAuth, usersController.updateUser)
-router.delete('/api/users/delete/:id', ensureAuth, usersController.deleteUser)
+router.put('/api/users/edit/:id', usersController.updateUser)
+router.delete('/api/users/delete/:id', usersController.deleteUser)
 
 // //Inventory Endpoints
 router.get('/api/inventory', inventoryController.getInventory)
 router.get('/api/inventory/:id', inventoryController.getItem)
 router.post('/api/inventory/new', inventoryController.addItem)
-router.put('/api/inventory/edit/:id', ensureAuth, inventoryController.updateItem)
-router.delete('/api/inventory/delete/:id', ensureAuth, inventoryController.deleteItem)
+router.put('/api/inventory/edit/:id', inventoryController.updateItem)
+router.delete('/api/inventory/delete/:id', inventoryController.deleteItem)
 
 // //Reservation Endpoints
 router.get('/api/reservations', reservationsController.getReservations)
 router.get('/api/reservations/:id', reservationsController.getReservation)
 router.post('/api/reservations/new', reservationsController.addReservation)
 router.put('/api/reservations/edit/:id', reservationsController.updateReservation)
-router.delete('/api/reservations/delete/:id', ensureAuth, reservationsController.deleteReservation)
+router.delete('/api/reservations/delete/:id', reservationsController.deleteReservation)
 
 module.exports = router
